222cc6b97892a31b25b71c172bbff9c5cfaa1a7c
Evgen Bielich <belichevgenij@gmail.com>
2021-02-17 16:41:11 +0200

    Update modul2_exampl.js

--- js/modul2_exampl.js
+++ js/modul2_exampl.js
@@ -44,10 +44,10 @@
 // let total = 0;
 // 1 Перебрать массив
 // for (let i = 0; i < cart.length; i += 1) {
-   // console.log(cart[i]);
+// console.log(cart[i]);
 
 // 3 каждый эл-т приплюсовать  к total
-   // total += cart[i];
+// total += cart[i];
 // }
 
 // Через for ... of - проще меньше кода------------------------------------------------
@@ -59,12 +59,11 @@
 
 // добавить такс-----------------------------------------------------------------------
 
-// for (let i = 0; i < cart.length; i += 1) { 
+// for (let i = 0; i < cart.length; i += 1) {
 //    cart[i] = Math.round(cart[i] * 1.1);
 // }
 
 // console.log(cart);
-
 
 // for (let value of cart) {
 // value = Math.round(value * 1.1);
@@ -72,22 +71,20 @@
 
 // console.log(cart);
 
-
-// ! Напиши скрипт который подсчитвает сумму всех четных чисел в массиве
-
+// ! Напиши скрипт который подсчитвает сумму всех четных чисел в массиве-----------------------------
 
 // const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
 
-   //  1 Переменная total
+//  1 Переменная total
 // let total = 0;
-   // 2 Перебрать массив
+// 2 Перебрать массив
 // for (let i = 0; i < numbers.length; i += 1) {
 //    const number = numbers[i];
 //    console.log(number);
-   // 3 на каждой итерации проверить эл-т на четность
-   // if (number % 2 === 0) {
-   //    console.log('Четное!!!!');
-   // 4 если четное плюсуем к total
+// 3 на каждой итерации проверить эл-т на четность
+// if (number % 2 === 0) {
+//    console.log('Четное!!!!');
+// 4 если четное плюсуем к total
 //       total += numbers[i];
 //    }
 // }
@@ -106,7 +103,6 @@
 
 // console.log("Total: ", total);
 
-
 // ! написать скрипт поиска логина -------------------------------------------------------------------
 
 /*- Если логина нет, вывести - "Пользователь [логин] не найден."
@@ -116,7 +112,6 @@
 - потом через for...of
 - логика break
 - метод includes() с тернанрым оператором */
-
 
 // const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
 // const loginToFind = 'poly1scute';
@@ -156,7 +151,6 @@
 
 // console.log(message);
 
-
 // ! Написать скрипт поиска самого маленького числа в массиве,----------------------------------------------------------------
 // при условии что числа уникальные (не повторяются).
 
@@ -170,7 +164,6 @@
 //    }
 // }
 // console.log("smallestNumber: ", smallestNumber);
-
 
 // ! Написать скрипт , который обьединяет все эл-ты массива в одно строковое значение -----------------------------------------------------
 // эл-тов может быть произвольное кол-во.
@@ -197,7 +190,6 @@
 // const string = friends.join(', ');
 // console.log(string);
 
-
 // ! Скрипт который заменяет регистр каждого символа на противополодный----------------------------------------------------------
 // например, если строка "JavaScript", то на выходе "jAVAsCRIPT"
 
@@ -212,23 +204,22 @@
 
 // устаревший способ
 
-   // if (letter === letter.toLocaleLowerCase()) {
-   //    console.log('Эта буква в нижнем регистре! - ', letter);
+// if (letter === letter.toLocaleLowerCase()) {
+//    console.log('Эта буква в нижнем регистре! - ', letter);
 
-   //    invertedString += letter.toLocaleUpperCase();
-   // } else {
-   //    console.log('Эта буква в верхнем регистре! - ', letter);
-   //    invertedString += letter.toLocaleLowerCase();
-   // }
+//    invertedString += letter.toLocaleUpperCase();
+// } else {
+//    console.log('Эта буква в верхнем регистре! - ', letter);
+//    invertedString += letter.toLocaleLowerCase();
+// }
 
 // Бoлее легкий способ
-   
+
 //    const isEqual = letter === letter.toLowerCase();
 //    invertedString += isEqual ? letter.toUpperCase() : letter.toLocaleLowerCase();
 // }
 
 // console.log(invertedString);
-
 
 // ! Дeлаем slug в URL из названия статьи(например на dev.to) ----------------------------------------------------------------------
 // Заголовок статьи состоит только из букв и пробелов
@@ -254,7 +245,6 @@
 // const slug1 = title.toLocaleLowerCase().split(' ').join('-');
 // console.log(slug1);
 
-
 // ! скрипт который считает сумму элементов двух массивов---------------------------------------------------------------
 
 // const array1 = [5, 10, 15, 20];
@@ -269,17 +259,15 @@
 
 // console.log(total);
 
-
 // ! РАботаем с коллекцией карточек в ТРелло -----------------------------------------------------------------------
-   // - метод splice()
-   // - удалить
-   // - добавить
-   // - обновить
+// - метод splice()
+// - удалить
+// - добавить
+// - обновить
 
 // const cards = ["Карточка-1", "Карточка-2", "Карточка-3", "Карточка-4", "Карточка-5"];
 
 // console.table(cards);
-
 
 // удаление (по индексу), метод indexOf()-----------------------------
 
@@ -290,7 +278,6 @@
 // cards.splice(index, 1);
 // console.table(cards);
 
-
 // Добавление (по индексу)---------------------------------------------
 
 // const cardToInterest = 'Карточка-6';
@@ -298,7 +285,6 @@
 
 // cards.splice(index, 0, cardToInterest);
 // console.table(cards);
-
 
 // обновление (по индексу)--------------------------------------------
 
@@ -309,9 +295,7 @@
 // cards.splice(index, 1, 'Обновленная карточка-4');
 // console.table(cards);
 
-
-
-// ! ФУНКЦИИ!!!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ! ФУНКЦИИ!!!---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*const add = function (x, y) {
    console.log(x);
@@ -327,10 +311,10 @@
 const r1 = add(5, 3);
 console.log('r1 = ', r1);*/
 
-// return - возврат, прекращение функции 
+// return - возврат, прекращение функции
 
 // const fn = function () {
-   
+
 //    console.log(1);
 //    console.log(2);
 //    return 555;
@@ -338,7 +322,6 @@ console.log('r1 = ', r1);*/
 // };
 
 // console.log(fn());
-
 
 // ! написать функцию calcuLateTotalPrice(items)-----------------------------------------------
 // которая принимает массив цен(чисел) и возвращает их сумму
@@ -351,7 +334,6 @@ console.log('r1 = ', r1);*/
 // }
 
 // console.log('Total: ', total);
-
 
 // через функцию ---------------------------------------------------------------------
 
@@ -383,28 +365,181 @@ console.log('r1 = ', r1);*/
 // logItems([1, 2, 3, 4, 5,]);
 // logItems(['клавиатура', "наушники", "часы"]);
 
-
 // ! Функция  findLogin(allLogins, login) для поиска логина---------------------------------------------
 
-const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
 // const loginToFind = 'poly1scute';
 
 // const message = logins.includes(loginToFind) ? `Пользователь ${loginToFind} найден.` : `Пользователь ${loginToFind} не найден.`;
 
 // console.log(message);
 
-const findLogin = function (allLogins, loginToFind) {
 
-   for (const login of allLogins) {
-      if (login === loginToFind) {
-         return `пользователь ${loginToFind} найден.`;
-      }
+// через фор-оф-------------------------------------------------------------------------------------
+
+// const findLogin = function (allLogins, loginToFind) {
+
+//    for (const login of allLogins) {
+//       if (login === loginToFind) {
+//          return `пользователь ${loginToFind} найден.`;
+//       }
+//    }
+
+//    return `пользователь ${loginToFind} нe найден.`;
+// };
+
+// функция через тернанрый оператор --------------------------------------------------------------
+
+// const findLogin = function (allLogins, loginToFind) {
+//    return allLogins.includes(loginToFind)
+//       ? `Пользователь ${loginToFind} найден.`
+//       : `Пользователь ${loginToFind} не найден.`;
+// };
+
+// console.log(findLogin(logins, 'dfdfsgdfhg432'));
+// console.log(findLogin(logins, 'k1widab3st'));
+// console.log(findLogin(logins, 'dfdffhg'));
+// console.log(findLogin(logins, 'aj4xth3m4n'));
+
+
+// ! Написать функцию findSmallesNumber поиска самого маленького числа в массиве,----------------------------------------------------------------
+// при условии что числа уникальные (не повторяются).
+
+
+// const findSmallesNumber = function (numbers) {
+//    let smallestNumber = numbers[0];
+
+//    for (const number of numbers) {
+
+//       if (number < smallestNumber) {
+//          smallestNumber = number;
+//       }
+//    }
+
+//    return smallestNumber;
+// }
+
+// console.log(findSmallesNumber([3, 8, 12, -2, 15]));
+// console.log(findSmallesNumber([100,54,8,12,47]));
+// console.log(findSmallesNumber([7,21,84,15,4]));
+
+
+// ! функция changeCase(string) которая заменяет регистр каждого символа на противоположный----------------------------------------------------------
+// например, если строка "JavaScript", то на выходе "jAVAsCRIPT"
+
+// const string = 'JavaScript';
+// const letters = string.split('');
+// let invertedString = '';
+
+// for (const letter of letters) {
+//    const isEqual = letter === letter.toLowerCase();
+//    invertedString += isEqual ? letter.toUpperCase() : letter.toLocaleLowerCase();
+// }
+
+// console.log(invertedString);
+
+// --------------------------------------------------------------------------------------------------
+
+// const changeCase = function (string) {
+//    const letters = string.split('');
+//    let invertedString = '';
+
+//    for (const letter of letters) {
+//       const isInLowerCase = letter === letter.toLowerCase();
+
+//       invertedString += isInLowerCase ? letter.toUpperCase() : letter.toLocaleLowerCase();
+//    }
+
+//    return invertedString;
+// }
+
+// console.log(changeCase('jfjkLKJNnBGJ'));
+// console.log(changeCase('AAAaaaAAAaaa'));
+
+
+// !Функция  slugify(string) получает строку и возвращает  в URL из названия статьи----------------------------------------------------------------------
+// статья состоит только из букв и пробелов
+
+
+// const title = 'Top 10 benefits of React framework';
+
+
+// const slug1 = title.toLocaleLowerCase().split(' ').join('-');
+// console.log(slug1);
+
+// --------------------------------------------------------------------------------------------------
+
+// const slugify = function (string) {
+//    return string.toLocaleLowerCase().split(' ').join('-');
+
+// }
+
+// console.log(slugify('Top 10 benefits of React framework'));
+// console.log(slugify('Azure Statics Web Apps are awesome'));
+
+
+// ! Псевдомассив arguments и Array.from и ....
+
+
+// const fn = function () {
+//    console.log(arguments);
+
+//    const args = Array.from(arguments);
+
+//    console.log(args);
+// }
+
+
+// rest- остаток -----------------------------------------------------------------------------------
+
+// const fn = function (...args) {
+//    console.log(args);
+// }
+
+// fn(1, 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
+
+// ! функция add для сложения произвольного кол-ва аргументов(чисел)
+// ! операция  ....(reset)--------------------------------------------------------------------------
+
+// const add = function (...args) {
+//    console.log(args);
+//    let total = 0;
+
+//    for (const arg of args) {
+//       total += arg;
+//    }
+
+//    return total;
+// }
+
+// console.log(add(1, 2, 3));
+// console.log(add(1, 2, 3, 4, 5));
+
+
+// ! функция filterNumbers(array [, number1, ...]) которая :-----------------------------------------
+   // - первым аругментом принимает массив чиселж
+   // - после первого аргумента может быть произвольное количество других аргументов которые будут числами.
+   // - функия должна вернуть новый массив , в котором будут только те аргументы, начиная со второго, для которых есть аналог в оригинальном массиве
+
+
+const filterNumbers = function (array, ...args) {
+   console.log(array);
+   console.log(args);
+   const uniqeElements = [];
+   
+   for (const element of array) {
+      if (args.includes(element)) {
+         uniqeElements.push(element);
+
+         console.log(`${element} есть везде`);
+      }  
    }
 
-   return `пользователь ${loginToFind} нe найден.`;
-};
+   return uniqeElements;
+   }
 
-console.log(findLogin(logins, 'dfdfsgdfhg432'));
-console.log(findLogin(logins, 'k1widab3st'));
-console.log(findLogin(logins, 'dfdffhg'));
-console.log(findLogin(logins, 'aj4xth3m4n'));
+
+console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8));
+console.log(filterNumbers([10,15,25,30], 23,30,18,15));
